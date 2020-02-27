354aba13b67064a82fd5c764aab4abb0b8a06b8c
dictator-x <dictator.xwu@gmail.com>
2020-02-27 18:49:42 -0500

    add ip test

---
+++ src/test/java/com/ebigdata/hadoop/mr/project/utils/IPTest.java
@@ -0,0 +1,16 @@
+package com.ebigdata.hadoop.mr.project.utils;
+
+import org.junit.Test;
+
+public class IPTest {
+
+    @Test
+    public void testIp() {
+
+        IPParser.RegionInfo regionInfo = IPParser.getInstance().analyseIp("123.116.60.97");
+
+        System.out.println(regionInfo.getCountry());
+        System.out.println(regionInfo.getProvince());
+        System.out.println(regionInfo.getCity());
+    }
+}
