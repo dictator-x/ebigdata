32ae2bdf6ad096792d91c006558f42b7dfea08fd
dictator-x <dictator.xwu@gmail.com>
2020-02-27 20:52:05 -0500

    ProvinceStatApp.java

--- src/main/java/com/ebigdata/hadoop/mr/project/mr/ProvinceStatApp.java
+++ src/main/java/com/ebigdata/hadoop/mr/project/mr/ProvinceStatApp.java
@@ -3,27 +3,30 @@ package com.ebigdata.hadoop.mr.project.mr;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
 
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
-
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Job;
-
-import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.apache.hadoop.conf.Configuration;
+
 import java.io.IOException;
+import java.util.Map;
 
-public class PVStatApp {
+import com.ebigdata.hadoop.mr.project.utils.LogParser;
+import com.ebigdata.hadoop.mr.project.utils.IPParser;
+
+public class ProvinceStatApp {
     public static void main(String[] args) throws Exception {
-
         Configuration configuration = new Configuration();
 
-        String dest = "output/v1/pvstat";
+        String dest = "output/v1/provincestat";
 
         FileSystem fileSystem = FileSystem.get(configuration);
         Path outputPath = new Path(dest);
@@ -33,7 +36,7 @@ public class PVStatApp {
         }
 
         Job job = Job.getInstance(configuration);
-        job.setJarByClass(PVStatApp.class);
+        job.setJarByClass(ProvinceStatApp.class);
 
         job.setMapperClass(MyMapper.class);
         job.setReducerClass(MyReducer.class);
@@ -41,7 +44,7 @@ public class PVStatApp {
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(LongWritable.class);
 
-        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(LongWritable.class);
 
         FileInputFormat.setInputPaths(job, new Path("input/raw/trackinfo_20130721.data"));
@@ -52,27 +55,51 @@ public class PVStatApp {
 
     static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 
-
-        private Text KEY = new Text("key");
         private LongWritable ONE = new LongWritable(1);
+        private LogParser logParser;
+        private Text nullText = new Text("-");
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            logParser = new LogParser();
+        }
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            context.write(KEY, ONE);
+            String log = value.toString();
+
+            Map<String, String> infos = logParser.parse(log);
+
+            String ip = infos.get("ip");
+
+            if ( StringUtils.isNotBlank(ip) ) {
+                IPParser.RegionInfo regionInfo = IPParser.getInstance().analyseIp(ip);
+                if ( regionInfo != null ) {
+                    String province = regionInfo.getProvince();
+                    if ( StringUtils.isNotBlank(province) ) {
+                        context.write(new Text(province), ONE);
+                    } else {
+                        context.write(nullText, ONE);
+                    }
+                } else {
+                    context.write(nullText, ONE);
+                }
+            } else {
+                context.write(nullText, ONE);
+            }
         }
     }
 
-    static class MyReducer extends Reducer<Text, LongWritable, NullWritable, LongWritable> {
+    static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
 
         @Override
         protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
-
             long count = 0;
             for ( LongWritable value : values ) {
                 count++;
             }
 
-            context.write(NullWritable.get(), new LongWritable(count));
+            context.write(key, new LongWritable(count));
         }
     }
 }
---
+++ src/main/java/com/ebigdata/hadoop/mr/project/utils/LogParser.java
@@ -0,0 +1,39 @@
+package com.ebigdata.hadoop.mr.project.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class LogParser {
+
+    public Map<String, String> parse(String log) {
+
+        Map<String, String> info = new HashMap<String, String>();
+
+        IPParser ipParser = IPParser.getInstance();
+
+        if ( StringUtils.isNotBlank(log) ) {
+            String[] splits = log.split("\001");
+
+            String ip = splits[13];
+            String province = "-";
+            String country = "-";
+            String city = "-";
+            IPParser.RegionInfo regionInfo = ipParser.analyseIp(ip);
+
+            if ( regionInfo != null ) {
+                country = regionInfo.getCountry();
+                province = regionInfo.getProvince();
+                city = regionInfo.getCity();
+            }
+
+            info.put("ip", ip);
+            info.put("country", country);
+            info.put("province", province);
+            info.put("city", city);
+        }
+
+        return info;
+    }
+}
